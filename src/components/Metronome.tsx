d2222df66676509e596b071780be24aa221658bf
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-17 19:49:44 -0400

    Fix blinking box (bpm transition needs fix)

--- src/components/Metronome.tsx
+++ src/components/Metronome.tsx
@@ -1,31 +1,75 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { Box, Slider, Typography } from "@mui/material";
 import * as Tone from 'tone';
 import tickSound from "../assets/Synth_Sine_C_lo.wav";
 import tickSoundDown from "../assets/Synth_Sine_C_hi.wav";
 
+const BlinkingSquare = ({ isBlinking }) => {
+  const squareStyle = {
+    width: "50px",
+    height: "50px",
+    backgroundColor: isBlinking ? "gray" : "white",
+    // transition: "background-color 0.2s ease-in-out",
+  };
+
+  return <div id='sq' style={squareStyle} />;
+};
 
 const Metronome = () => {
   const [beat, setBeat] = useState(120);
   const [isBlinking, setIsBlinking] = useState(false);
-  // const player = new Tone.Player(tickSound).toDestination();
   // const playerDown = new Tone.Player(tickSoundDown).toDestination();
 
   // let repeat = Tone.Transport.scheduleRepeat((time) => {
-  //   player.start(time).stop(time + 0.1);
+  //   sampler.triggerAttackRelease("c4", "32n", time);
   // }, "4n");
+
+  // const seq = new Tone.Sequence((time, note) => {
+  //   sampler.triggerAttackRelease(note, "32n", time);
+  // }, ["C4", "C3", "C3", "C3"], "16n").stop();
+
+  // useEffect( () => {
+  //   // Create audio components and set up scheduling only once
+  //   const sampler = new Tone.Sampler({
+  //     urls: {
+  //       C3: tickSound,
+  //       C4: tickSoundDown,
+  //     },
+  //     onload: () => {
+  //       // sampler.triggerAttackRelease("C6", "32n", 0);
+  //       // Tone.Transport.scheduleRepeat((time) => {
+  //       //   setIsBlinking(false);
+  //       //   sampler.triggerAttackRelease("C4", "32n", time);
+  //       //   Tone.Draw.schedule(() => setIsBlinking(true), time);
+  //       // }, "4n");
+  //       // new Tone.Sequence((time, note) => {
+  //       //   sampler.triggerAttackRelease(note, "32n", time);
+  //       // }, ["C4", "C3", "C3", "C3"], "16n").start(0);
+  //     }
+  //   }).toDestination();
+
+  //   Tone.Transport.bpm.value = beat;
+
+  //   // Tone.Transport.scheduleRepeat((time) => {
+  //   //   setIsBlinking(false);
+  //   //   sampler.triggerAttackRelease("C5", "32n", time);
+  //   //   Tone.Draw.schedule(() => setIsBlinking(true), time);
+  //   // }, "4n");
+
+  //   return () => {
+  //     // Clean up audio components if needed
+  //     sampler.dispose();
+  //   };
+  // }, [beat]);
 
   const sampler = new Tone.Sampler({
     urls: {
       C3: tickSound,
       C4: tickSoundDown,
     },
-    // baseUrl: "https://tonejs.github.io/audio/casio/",
-    // onload: () => {
-    //   sampler.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
-    // }
   }).toDestination();
-  const synthA = new Tone.Synth().toDestination();
+
+  // const synthA = new Tone.Synth().toDestination();
 
   // const loopA = new Tone.Loop(time => {
   //   // synthA.triggerAttackRelease("C2", "8n", time);
@@ -37,49 +81,58 @@ const Metronome = () => {
   //   playerDown.start(time);
   // }, "4n").start(0);
 
-  const seq = new Tone.Sequence((time, note) => {
-    sampler.triggerAttackRelease(note, "32n", time);
-    // synthA.triggerAttackRelease(note, "32n", time);
-  }, ["C4", "C3", "C3", "C3"], "16n").start(0);
-  Tone.Transport.bpm.value = beat;
-  Tone.Transport.start();
+  // const seq = new Tone.Sequence((time, note) => {
+  //   sampler.triggerAttackRelease(note, "32n", time);
+  //   // synthA.triggerAttackRelease(note, "32n", time);
+  // }, ["C4", "C3", "C3", "C3"], "16n").start(0);
+  // Tone.Transport.bpm.value = beat;
+
+
+  // Tone.Transport.start();
+
+  // Tone.Transport.schedule((time) => {
+  //   // use the time argument to schedule a callback with Draw
+  //   Tone.Draw.schedule(() => {
+  //     // do drawing or DOM manipulation here
+  //     console.log(time);
+  //   }, time);
+  // }, "1m");
+  // Tone.Transport.start();
+  // Tone.Transport.scheduleRepeat(time => {
+  //   Tone.Draw.schedule(() => setIsBlinking(true), time);
+  //   Tone.Draw.schedule(() => setIsBlinking(false), time + 1);
+  // }, '4n');
 
   function start() {
+    const seq = new Tone.Sequence((time, note) => {
+      sampler.triggerAttackRelease(note, "32n", time);
+    }, ["C4", "C3", "C3", "C3"], "16n").stop();
+
+    Tone.Transport.scheduleRepeat((time) => {
+      setIsBlinking(false);
+      seq.start();
+      // sampler.triggerAttackRelease("C4", "16n", time);
+      Tone.Draw.schedule(() => setIsBlinking(true), time);
+    }, "4n");
+
     Tone.Transport.start();
   }
+
   function stop() {
+    Tone.Transport.cancel();
+    // seq.cancel();
+    // seq.stop();
+    // seq.dispose();
     Tone.Transport.stop();
   }
 
-  // setIsBlinking(true);
-  // setTimeout(() => {
-  //   setIsBlinking(false);
-  // }, 100); // Adjust this value to control the blink duration
-
-  const squareStyle = {
-    width: "50px",
-    height: "50px",
-    backgroundColor: "gray",
-    transition: "background-color 0.2s ease-in-out",
-  };
-
-  const blinkStyle = {
-    backgroundColor: "white",
-  };
-
   const handleSliderChange = (event: Event, newValue: number) => {
-    // stop()
-    // loopA.cancel()
-    // loopB.cancel()
-    Tone.Transport.cancel();
-    // Tone.Transport.stop()
-    // Tone.Transport.dispose()
-    // new Tone.Transport
-    // repeat = undefined;
+    stop()
     setBeat(newValue);
     setTimeout(() => {
       Tone.Transport.bpm.setValueAtTime(newValue, Tone.Transport.now())
     }, 1000)
+    start()
   };
 
   useEffect(() => {
@@ -97,10 +150,12 @@ const Metronome = () => {
     }
   }, []);
 
+  const blinkingSquare = useMemo(() => <BlinkingSquare isBlinking={isBlinking} />, [isBlinking]);
 
   return (
     <div className="metronome">
-      <div style={{ ...squareStyle, ...(isBlinking ? blinkStyle : {}) }} />
+      {/* <div id='sq' style={{ ...squareStyle, ...(isBlinking ? blinkStyle : {}) }} /> */}
+      {blinkingSquare}
       <Slider
         min={60}
         max={256}
