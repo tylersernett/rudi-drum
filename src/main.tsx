c3c5615958c7acb8b8547135fcbf537325de2593
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-19 21:15:02 -0400

    Add basic theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { orange, grey} from '@mui/material/colors';
+import CssBaseline from "@mui/material/CssBaseline";
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+    mode: 'dark',
+    background: {
+      default: grey[900]
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
