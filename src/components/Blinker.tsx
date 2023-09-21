5a906585796c4af5685d49302ae1ea09d791b3ac
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-20 23:59:09 -0400

    Add toggle

--- src/components/Blinker.tsx
+++ src/components/Blinker.tsx
@@ -1,7 +1,7 @@
 // import { useState, useEffect, useMemo, useRef } from "react";
 // import * as Tone from 'tone';
-import { Dispatch, SetStateAction } from "react";
-import { Box } from "@mui/material";
+import { Dispatch, SetStateAction, useState } from "react";
+import { Box, Switch, Typography } from "@mui/material";
 
 interface BlinkingSquareProps {
   isBlinking: boolean;
@@ -9,11 +9,14 @@ interface BlinkingSquareProps {
 }
 
 const Blinker: React.FC<BlinkingSquareProps> = ({ isBlinking, setIsBlinking }) => {
+  const [blinkToggle, setBlinkToggle] = useState(true);
+
   const squareStyle = {
     width: "50px",
     height: "50px",
     backgroundColor: isBlinking ? "orange" : "lightgray",
     borderRadius: '100%',
+    visibility: blinkToggle ? "visible" : "hidden",
     // transition: "background-color 0.2s ease-in-out",
   };
 
@@ -23,15 +26,27 @@ const Blinker: React.FC<BlinkingSquareProps> = ({ isBlinking, setIsBlinking }) =
     }, 60);
     // Tone.Draw.schedule(() => setIsBlinking(false), Tone.now() +Tone.Time("32n").toSeconds());
   }
+  const handleToggleChange = () => {
+    setBlinkToggle((prevBlinkToggle) => !prevBlinkToggle);
+  };
+
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      mt={2}
-    >
-      <div id='sq' style={squareStyle} />
-    </Box>
+    <>
+      <Switch
+        checked={blinkToggle}
+        onChange={handleToggleChange}
+        color="primary"
+      />
+      <Typography>Blink Toggle: {blinkToggle ? 'On' : 'Off'}</Typography>
+      <Box
+        display="flex"
+        justifyContent="center"
+        // mt={2}
+      >
+        <div id='sq' style={squareStyle} />
+      </Box>
+    </>
   )
 };
 
