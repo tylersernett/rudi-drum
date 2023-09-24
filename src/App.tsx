604393471ec9189e190233bd3e4adfbb06eb0b91
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-24 03:04:12 -0400

    Add basic Grader

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,24 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Metronome from './components/Metronome';
-import Toney from './components/Toney';
 import { Box, Typography } from '@mui/material';
+import * as Tone from 'tone';
 
 function App() {
-  const [countL, setCountL] = useState(0);
-  const [countR, setCountR] = useState(0);
-
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === 'F' || event.key === 'f') {
-        setCountL((prevCount) => prevCount + 1);
-      } else if (event.key === 'J' || event.key === 'j') {
-        setCountR((prevCount) => prevCount + 1);
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
 
   return (
     <>
-      {/* <Box mt={-3}>
+      <Box mt={-8}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </Box> */}
+      </Box>
 
-      <Typography mb={-2} variant='h1'>rudi</Typography>
-      <div className="card">
-        <span onClick={() => setCountL((count) => count + 1)}>
-          Left: {countL}
-        </span>&nbsp;|&nbsp;
-        <span onClick={() => setCountR((count) => count + 1)}>
-          Right: {countR}
-        </span>
-        <Metronome />
-      </div>
+      <Typography variant='h1'>rudi</Typography>
+      <Metronome />
     </>
   );
 }
---
+++ src/components/Grader.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import * as Tone from 'tone';
+
+
+const Grader = () => {
+  const [countL, setCountL] = useState(0);
+  const [countR, setCountR] = useState(0);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'F' || event.key === 'f') {
+        setCountL((prevCount) => prevCount + 1);
+        calculateGrade();
+      } else if (event.key === 'J' || event.key === 'j') {
+        setCountR((prevCount) => prevCount + 1);
+        calculateGrade();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  const calculateGrade = () => {
+    const nextQuarterNoteTime = Tone.Transport.nextSubdivision("4n")
+    const immediateTime = Tone.immediate()
+    const timeToNextQuarter = nextQuarterNoteTime - immediateTime;
+    console.log('now to next 4n: ', timeToNextQuarter)
+    const quarterNoteLength = Tone.Transport.toSeconds("4n");
+    const lookAhead = 0.0;
+    console.log('score: ', quarterNoteLength - timeToNextQuarter - lookAhead, " | ", (timeToNextQuarter + lookAhead) / quarterNoteLength)
+  }
+
+  return (
+    <Box>
+      <span onClick={() => setCountL((count) => count + 1)}>
+        Left: {countL}
+      </span>&nbsp;|&nbsp;
+      <span onClick={() => setCountR((count) => count + 1)}>
+        Right: {countR}
+      </span>
+    </Box>
+  )
+}
+
+export default Grader
