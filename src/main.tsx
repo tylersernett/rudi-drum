3ebd67295035c420fcf5999c64125800f85c28bb
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-25 15:48:46 -0400

    Extract theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,37 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { orange, grey, purple, } from '@mui/material/colors';
 import CssBaseline from "@mui/material/CssBaseline";
-
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: orange[500],
-    },
-    mode: 'dark',
-    background: {
-      default: grey[900]
-    }
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          '&:focus': {
-            // boxSizing: 'border-box',
-            outline: '1px solid white', // Change the outline color to purple when focused
-            // border: `1px solid ${purple.A200}`, // Change the outline color to purple when focused
-            boxShadow: `inset 0px 0px 0px 2px ${purple.A200}`,
-            transition: 'none'
-          },
-        },
-      },
-    },
-  },
-
-});
+import { theme } from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/theme.ts
@@ -0,0 +1,42 @@
+import { createTheme } from "@mui/material";
+import { orange, grey, purple, } from '@mui/material/colors';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+    mode: 'dark',
+    background: {
+      default: grey[900]
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          '&:focus': {
+            // boxSizing: 'border-box',
+            outline: '1px solid white', // Change the outline color to purple when focused
+            // border: `1px solid ${purple.A200}`, // Change the outline color to purple when focused
+            boxShadow: `inset 0px 0px 0px 2px ${purple.A200}`,
+            transition: 'none'
+          },
+        },
+      },
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          '&:focus': {
+            // boxSizing: 'border-box',
+            outline: '1px solid white', // Change the outline color to purple when focused
+            // border: `1px solid ${purple.A200}`, // Change the outline color to purple when focused
+            boxShadow: `inset 0px 0px 0px 2px ${purple.A200}`,
+            transition: 'none'
+          },
+        },
+      },
+    },
+  },
+});
