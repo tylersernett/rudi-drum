2b9639c1795716d320aa3917b3ec1581cfd63c24
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-26 01:28:32 -0400

    Fix URL

--- src/services/login.ts
+++ src/services/login.ts
@@ -3,7 +3,7 @@ import { API_URL } from '../../config'
 import { LoginCredentials } from '../types'
 
 const login = async (credentials: LoginCredentials) => {
-  const response = await axios.post(API_URL, credentials)
+  const response = await axios.post(`${API_URL}/login`, credentials)
   return response.data
 }
 
