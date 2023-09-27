d8fa58b72b0614489b1ea4a6223d7d397b4361c6
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-26 23:48:58 -0400

    Add UserMenu

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { Badge, Box, IconButton, Link, Typography, AppBar, Container, Toolbar, Menu, Button, MenuItem, useMediaQuery } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import { Typography, AppBar, Container, Toolbar, Button, } from "@mui/material";
 import { theme } from "../theme";
 import LoginModal from "./LoginModal";
 import { useUserContext } from '../context/UserContext';
-import logoutService from "../services/logout";
+import MainMenu from "./MainMenu";
+import UserMenu from "./UserMenu";
 
 const TopBar = () => {
-  const { username, bearerToken, clearUserInfo } = useUserContext();
+  const { username } = useUserContext();
   const [openLoginModal, setOpenLoginModal] = useState(false);
 
   const handleOpenLoginModal = () => {
@@ -18,34 +18,15 @@ const TopBar = () => {
     setOpenLoginModal(false);
   };
 
-  const handleLogout = async () => {
-    console.log('LOGOUT attempt')
-    try {
-      await logoutService.logout(bearerToken)
-      console.log('logout success')
-      clearUserInfo()
-    } catch (error) {
-      console.log('error logging out', error)
-    }
-  };
-
   return (
     <AppBar>
-      <Toolbar  >
-        <IconButton
-          size="large"
-          edge="start"
-          color="primary"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
+      <Toolbar >
+        <MainMenu />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Placeholder
         </Typography>
         {username ? (
-          <Button color="primary" onClick={handleLogout}><Typography variant='body2'>Logout</Typography></Button>
+          <UserMenu />
         ) : (
           <Button color="primary" onClick={handleOpenLoginModal}><Typography variant='body2'>Login</Typography></Button>
         )}
---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,58 @@
+import { IconButton, Menu, MenuItem, Typography } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useState } from 'react';
+import logoutService from "../services/logout";
+import { useUserContext } from '../context/UserContext';
+
+const UserMenu = () => {
+  const { username, bearerToken, clearUserInfo } = useUserContext();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = async () => {
+    console.log('LOGOUT attempt')
+    try {
+      await logoutService.logout(bearerToken)
+      console.log('logout success')
+      clearUserInfo()
+    } catch (error) {
+      console.log('error logging out', error)
+    }
+  };
+
+  return (
+    <>
+      <IconButton
+        size="large"
+        edge="start"
+        color="primary"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={handleClick}
+      >
+        <AccountCircleIcon fontSize='large'/>
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <Typography variant='h5' mx={2} mb={1} color='primary'>{username}</Typography>
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
+  )
+}
+
+export default UserMenu
