9bc65763d7559b8f24b80db05dc85b4e28e7e7d8
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-26 22:53:54 -0400

    Add error notification

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormHelperText, TextField, } from '@mui/material';
 import loginService from '../services/login';
 import { useUserContext } from '../context/UserContext';
 interface LoginModalProps {
@@ -11,10 +11,12 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
   const { setUserInfo } = useUserContext();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleClose = () => {
     setUsername('');
     setPassword('');
+    setError('')
     onClose();
   };
 
@@ -28,40 +30,51 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
       setUserInfo(user.username, user.token)
       handleClose();
     } catch (error) {
+      setError('Login failed. Please check your credentials.');
       console.log(error)
     }
   }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    handleLogin();
+  };
 
   return (
-    <Dialog open={open} onClose={handleClose}>
+    <Dialog open={open} onClose={handleClose} maxWidth='xs' disableRestoreFocus>
       <DialogTitle>Login</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Username"
-          type="text"
-          fullWidth
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextField
-          margin="dense"
-          label="Password"
-          type="password"
-          fullWidth
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={handleLogin} color="primary">
-          Login
-        </Button>
-      </DialogActions>
+      <form onSubmit={handleSubmit}>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Username"
+            type="text"
+            fullWidth
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="Password"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControl fullWidth error={!!error}>
+            {error && <FormHelperText>{error}</FormHelperText>}
+          </FormControl>
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button color="primary" type='submit' >
+            Login
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
