ace5b778920bc72e457b1bf40879fac56a4945f1
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-10-03 22:27:17 -0400

    Assert env var as string

--- config.ts
+++ config.ts
@@ -1,7 +1,5 @@
-// config.js
-// const isDevelopment = import.meta.env.DEV;
 
-const API_URL = import.meta.env.VITE_REACT_APP_API_URL
+const API_URL = import.meta.env.VITE_REACT_APP_API_URL as string;
 console.log("apiurl", API_URL)
 
 export { API_URL };
