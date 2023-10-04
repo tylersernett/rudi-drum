e3c665d47b144fe8d52193c7629701d5a7b45ce0
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-10-03 22:26:41 -0400

    Add type for server return

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import { API_URL } from '../../config'
 import { LoginCredentials } from '../types'
+import { IUser } from '../types'
 
-const login = async (credentials: LoginCredentials) => {
-  const response = await axios.post(`${API_URL}/login`, credentials)
+const login = async (credentials: LoginCredentials): Promise<IUser> => {
+  const response = await axios.post<IUser>(`${API_URL}/login`, credentials)
   return response.data
 }
 
